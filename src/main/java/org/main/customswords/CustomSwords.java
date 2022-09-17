955b883ae42e6ad413e89f9a892b90b658cb1ab6
ZMizgalski <zbigniew.mizgalskifullstack@gmail.com>
2022-09-17 20:04:51 +0200

    First commit

---
+++ src/main/java/org/main/customswords/CustomSwords.java
@@ -0,0 +1,17 @@
+package org.main.customswords;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomSwords extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
